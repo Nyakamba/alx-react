25fa550ea6a8debff17cc3a79936dc4835322c9d
Nyakamba <omwegaeonkc@gmail.com>
2023-02-28 12:05:03 +0300

    task_1

---
+++ 0x02-react_props/task_1/dashboard/src/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+import logo from '../assets/logo.jpg';
+
+export default function Header() {
+  return (
+    <header className='header'>
+      <img src={logo} alt='logo' />
+      <h1>School dashboard</h1>
+    </header>
+  );
+}
