2ed9867d6a1b45384ab3aa167772a12ae83fa18e
Nyakamba Omwega Enock <omwegaenock@gmail.com>
2023-04-04 09:47:26 +0000

    uireducer

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
@@ -1,31 +1,45 @@
-import {
-	DISPLAY_NOTIFICATION_DRAWER,
-	HIDE_NOTIFICATION_DRAWER,
-	LOGIN_SUCCESS,
-	LOGIN_FAILURE,
-	LOGOUT,
-} from '../actions/uiActionTypes';
-import { Map } from 'immutable';
+import { Map } from "immutable";
+import { logout } from "../actions/uiActionCreators";
 
-const defaultState = {
-	isNotificationDrawerVisible: false,
-	isUserLoggedIn: false,
-	user: {},
+import {
+  LOGIN,
+  LOGOUT,
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+} from "../actions/uiActionTypes";
+
+export const initialState = {
+  isNotificationDrawerVisible: false,
+  isUserLoggedIn: false,
+  user: null,
 };
 
-export default function uiReducer(state = defaultState, action) {
-	switch (action.type) {
-		case DISPLAY_NOTIFICATION_DRAWER:
-			return { ...state, isNotificationDrawerVisible: true };
-		case HIDE_NOTIFICATION_DRAWER:
-			return { ...state, isNotificationDrawerVisible: false };
-		case LOGIN_SUCCESS:
-			return { ...state, isUserLoggedIn: true };
-		case LOGIN_FAILURE:
-			return { ...state, isUserLoggedIn: false };
-		case LOGOUT:
-			return { ...state, isUserLoggedIn: false };
-		default:
-			return state;
-	}
-}
+const uiReducer = (state = Map(initialState), action) => {
+  switch (action.type) {
+    case DISPLAY_NOTIFICATION_DRAWER:
+      return state.set("isNotificationDrawerVisible", true);
+
+    case HIDE_NOTIFICATION_DRAWER:
+      return state.set("isNotificationDrawerVisible", false);
+
+    case LOGIN_SUCCESS:
+      return state.set("isUserLoggedIn", true);
+
+    case LOGIN_FAILURE:
+      return state.set("isUserLoggedIn", false);
+
+    case LOGIN:
+      return state.set("user", action.user);
+
+    case LOGOUT:
+      return state.set("isUserLoggedIn", false).set("user", null);
+
+    default:
+      break;
+  }
+  return state;
+};
+
+export default uiReducer;
