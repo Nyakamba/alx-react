e04066371cbd2e4e5349b66ac6d6db7d5d23f461
Nyakamba <omwegaenock@gmail.com>
2023-08-27 08:51:16 +0300

    Notifications reducer

--- 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/uiReducer.js
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/uiReducer.js
@@ -1,31 +1,31 @@
 import {
-	DISPLAY_NOTIFICATION_DRAWER,
-	HIDE_NOTIFICATION_DRAWER,
-	LOGIN_SUCCESS,
-	LOGIN_FAILURE,
-	LOGOUT,
-} from '../actions/uiActionTypes';
-import { Map } from 'immutable';
+  DISPLAY_NOTIFICATION_DRAWER,
+  HIDE_NOTIFICATION_DRAWER,
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  LOGOUT,
+} from "../actions/uiActionTypes";
+import { Map } from "immutable";
 
 const defaultState = {
-	isNotificationDrawerVisible: false,
-	isUserLoggedIn: false,
-	user: {},
+  isNotificationDrawerVisible: false,
+  isUserLoggedIn: false,
+  user: {},
 };
 
 export default function uiReducer(state = defaultState, action) {
-	switch (action.type) {
-		case DISPLAY_NOTIFICATION_DRAWER:
-			return { ...state, isNotificationDrawerVisible: true };
-		case HIDE_NOTIFICATION_DRAWER:
-			return { ...state, isNotificationDrawerVisible: false };
-		case LOGIN_SUCCESS:
-			return { ...state, isUserLoggedIn: true };
-		case LOGIN_FAILURE:
-			return { ...state, isUserLoggedIn: false };
-		case LOGOUT:
-			return { ...state, isUserLoggedIn: false };
-		default:
-			return state;
-	}
+  switch (action.type) {
+    case DISPLAY_NOTIFICATION_DRAWER:
+      return { ...state, isNotificationDrawerVisible: true };
+    case HIDE_NOTIFICATION_DRAWER:
+      return { ...state, isNotificationDrawerVisible: false };
+    case LOGIN_SUCCESS:
+      return { ...state, isUserLoggedIn: true };
+    case LOGIN_FAILURE:
+      return { ...state, isUserLoggedIn: false };
+    case LOGOUT:
+      return { ...state, isUserLoggedIn: false };
+    default:
+      return state;
+  }
 }
