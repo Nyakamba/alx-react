9a6ce32eb253232ef184b6243c89046ef564e607
Nyakamba <omwegaenock@gmail.com>
2023-08-30 13:50:30 +0300

    Selectors

--- 0x08-react_redux_reducer_selector/task_5/dashboard/src/BodySection/BodySection.js
+++ 0x08-react_redux_reducer_selector/task_5/dashboard/src/BodySection/BodySection.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { StyleSheet, css } from 'aphrodite';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, css } from "aphrodite";
 
 const styles = StyleSheet.create({
   small: {
-    '@media (max-width: 900px)': {
-      display: 'grid',
-      justifyContent: 'center',
-      width: '50%',
-      textAlign: 'center',
-      marginLeft: 'auto',
-      marginRight: 'auto'
-    }
-  }
-})
+    "@media (max-width: 900px)": {
+      display: "grid",
+      justifyContent: "center",
+      width: "50%",
+      textAlign: "center",
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+  },
+});
 
 export default class BodySection extends Component {
   render() {
@@ -30,4 +30,4 @@ export default class BodySection extends Component {
 BodySection.propTypes = {
   title: PropTypes.string,
   // children: PropTypes.arrayOf(PropTypes.node)
-}
+};
