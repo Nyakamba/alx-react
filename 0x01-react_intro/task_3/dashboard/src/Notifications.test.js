439b1708740ab12ebabb8971259bc199a11dc8b3
Nyakamba <omwegaenock@gmail.com>
2024-01-31 10:00:03 +0300

    writting tests

--- 0x01-react_intro/task_3/dashboard/src/Notifications.test.js
+++ 0x01-react_intro/task_3/dashboard/src/Notifications.test.js
@@ -1,15 +1,15 @@
-import { shallow } from 'enzyme';
-import Notifications from './Notifications';
+import { shallow } from "enzyme";
+import Notifications from "./Notifications";
 
 const wrapper = shallow(<Notifications />);
 
-it('renders without crashing', () => {
+it("renders without crashing", () => {
   shallow(<Notifications />);
 });
-it('renders three list items', () => {
-  expect(wrapper.find('li').children().length).toEqual(3);
+it("renders three list items", () => {
+  expect(wrapper.find("li").children().length).toEqual(3);
 });
-it('renders the <p>', () => {
+it("renders the <p>", () => {
   expect(
     wrapper.containsMatchingElement(<p>Here is the list of notifications</p>)
   ).toBeTruthy();
