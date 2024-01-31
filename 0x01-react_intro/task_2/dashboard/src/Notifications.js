7a815936d6facac0288a16aee832e894c7a616b7
Nyakamba <omwegaenock@gmail.com>
2024-01-31 09:44:34 +0300

    notifications functionality

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,36 +1,36 @@
 import React from "react";
-import './Notifications.css'
-import { getLatestNotification } from './utils';
-import close_icon from './close-icon.png'
+import "./Notifications.css";
+import { getLatestNotification } from "./utils";
+import close_icon from "./close-icon.png";
 const Notifications = () => {
   return (
-    <div className='Notifications'>
+    <div className="Notifications">
       <p>Here is the list of notifications</p>
       <ul>
-        <li data-priority='default'>New course available</li>
-        <li data-priority='urgent'>New resume available</li>
-        <li data-priority='urgent'>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li data-priority="urgent">
           <div
             dangerouslySetInnerHTML={{ __html: `${getLatestNotification()}` }}
           ></div>
         </li>
       </ul>
-      <button 
+      <button
         type="button"
         aria-label="Close"
-        onClick={() => console.log('Close button has been clicked')}
+        onClick={() => console.log("Close button has been clicked")}
         style={{
-          display: 'inline-block',
-          position: 'absolute',
-          top: '16px',
-          right: '16px',
+          display: "inline-block",
+          position: "absolute",
+          top: "16px",
+          right: "16px",
           background: 0,
           border: 0,
-          outline: 'none',
-          cursor: 'pointer',
-        }}  
+          outline: "none",
+          cursor: "pointer",
+        }}
       >
-        <img src={close_icon} alt='' style={{ width: '8px', height: '8px' }} />
+        <img src={close_icon} alt="" style={{ width: "8px", height: "8px" }} />
       </button>
     </div>
   );
