5a1faf5846033d8591d20dcf16c085fa4e946af9
Nyakamba <omwegaenock@gmail.com>
2024-01-31 08:29:11 +0300

    react intro

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.jpg';
-import './App.css';
+import logo from "./logo.jpg";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <header className='App-header'>
-        <img src={logo} alt='logo' />
+      <header className="App-header">
+        <img src={logo} alt="logo" />
         <h1>School dashboard</h1>
       </header>
-      <main role='main' className='App-body'>
+      <main role="main" className="App-body">
         <p>Login to access the full dashboard</p>
       </main>
-      <footer className='App-footer'>
+      <footer className="App-footer">
         <p>Copyright 2020 - holberton School</p>
       </footer>
     </>
